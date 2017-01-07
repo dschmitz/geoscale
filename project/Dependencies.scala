4928b2ae8d2b6f25603ff37253bdf666c7fbc207
David Schmitz <David.Schmitz@audi.de>
2017-01-07 23:12:31 +0100

    update to scala 2.12.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,7 +1,7 @@
 import sbt._
 
 object Version {
-  final val Scala      = "2.12.0"
+  final val Scala      = "2.12.1"
   final val ScalaTest  = "3.0.1"
   final val ScalaCheck = "1.13.4"
   final val Jts        = "1.13"
